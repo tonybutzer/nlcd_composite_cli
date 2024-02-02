66f97be55f20ffb72abdb381fa61b93bb542a85f
AWS ParallelCluster user <ec2-user@ip-10-12-71-178.us-west-2.compute.internal>
2024-02-02 15:30:26 +0000

    auto commit from tony makefile nov2023

---
+++ 8_observe/sh.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+# tony
+
+import subprocess
+import pandas as pd
+from tabulate import tabulate
+
+def run_command(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    return result.stdout, result.stderr
+
+def parse_squeue_output(output):
+    # Assuming squeue output format: JOBID, PARTITION, NAME, USER, STATE, TIME, NODES, CPUS
+    data=[]
+    lines = output.strip().split('\n')
+    headers = lines[0].split()
+    for line in lines[1:]:
+        l_arr = line.split()
+        if (len(l_arr) > 8):
+            pass
+        else:
+            data.append(l_arr)
+    #data = [line.split() for line in lines[1:]]
+    df = pd.DataFrame(data, columns=headers)
+    return df
+
+def display_dataframe(df):
+    # Display the DataFrame using tabulate
+    print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
+
+def display_state_counts(df):
+    # Count occurrences of each unique state (ST)
+    state_counts = df['ST'].value_counts()
+
+    # Display the counts
+    print("\nState Counts:")
+    print(tabulate(state_counts.reset_index(), headers=['STATE', 'Count'], tablefmt='fancy_grid', showindex=False))
+
+def main():
+    # Run squeue command and get the output
+    squeue_output, _ = run_command('squeue --format="%14i %19P %14j %8u %2t %7M %5D %R"')
+
+    # Parse squeue output into a DataFrame
+    df = parse_squeue_output(squeue_output)
+
+    filtered_df = df[df['ST'] == 'R']
+
+    # Display the DataFrame nicely on the terminal
+    display_dataframe(filtered_df.head())
+
+    # Display state counts
+    display_state_counts(df)
+
+if __name__ == "__main__":
+    main()
+
+
